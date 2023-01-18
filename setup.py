824b67df7733850aac64538d738063b874bcec1c
kalana kithmina <e19198@eng.pdn.ac.lk>
2023-01-19 00:42:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tmwad',
-    version='0.1',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=['requests'],
     url='https://github.com/kalanakt/tmwad',
